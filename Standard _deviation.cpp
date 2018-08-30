810ca364a7aee73b9cde54d68c12928c33781d52
abded <abded@yandex.ru>
2018-08-30 13:19:19 +0300

    Считает стандартное отклонение.

---
+++ Standard _deviation.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main() {
+	int x, i(0), amount_squares(0);
+	double delta, amount(0), average, variable(0), difference;
+	while (cin >> x, x != 0) { // последовательность оканчивающаяся на 0; 0 не входит в последовательность
+		i = i + 1; // считает количество переменных
+		amount_squares = pow(x, 2) + amount_squares; // считает сумму квадратов
+		amount = x + amount; // считает сумму
+	}
+	delta = sqrt((amount_squares - (pow(amount, 2) / i)) / (i - 1)); // считаем стандартное отклонение
+	cout << setprecision(11) << fixed; // устанавливаем точность 11 знаков после запятой
+	cout << delta;
+	return 0;
+}
